396c37dd1d4c68450812097785fdc6135b45a1e0
maxprafferty <maxprafferty@gmail.com>
2015-05-29 14:27:04 -0400

    changed package name, removed bad quotes

--- customevent.js
+++ customevent.js
@@ -39,7 +39,7 @@
             }
         }
         for(var key in local) {
-            local['key']['CustomEvent'] = CustomEvent;
+            local[key]['CustomEvent'] = CustomEvent;
         }
     }
 
